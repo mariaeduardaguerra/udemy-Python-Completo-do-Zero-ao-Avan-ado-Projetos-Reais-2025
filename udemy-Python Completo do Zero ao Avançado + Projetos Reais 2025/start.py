10d22bb93d3e782b1e0975a2cf2e23a78c432e92
Maria Eduarda Cândido Alves Guerra <99684296+mariaeduardaguerra@users.noreply.github.com>
2025-01-16 22:27:31 -0300

    Add files via upload

---
+++ udemy-Python Completo do Zero ao Avançado + Projetos Reais 2025/start.py
@@ -0,0 +1,13 @@
+# nome = 'duda'
+# print(nome)
+
+# -------------- VARIÁVEIS PRIMITIVAS --------------
+
+idade = 38 # (Int) números inteiros
+peso = 80.4 # (Float) números decimais
+nome = 'Duda' # (String) caracteres
+ativo = True # (Bool) valores lógicos
+resultado = None # (None) ausência de valor
+
+print(type(idade))
+print(type(nome))
